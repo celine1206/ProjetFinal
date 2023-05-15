d9e270aae60ebe7d9f01b15faac5a78d684016dc
celine1206 <1924163@cegepoutaouais.qc.ca>
2023-05-15 09:14:10 -0400

    cration de les classes client et reparation

--- ProjetFinal.py
+++ ProjetFinal.py
@@ -1,3 +1,5 @@
+import datetime
+
 # cration de la classe Personne
 class Personne(object):
 
@@ -65,4 +67,84 @@ class Employe(Personne):
     #La méthode magique nommée "__str__".
     def __str__(self):
         return f'{super().__str__()}\n, {self.__code}\n, {self.__fonction}'
+
+
+#creation de la classe Client
+class Client(Personne):
+
+    #le constructeur
+    def __int__(self, nom : str, prenom : str, telephone : str, courriel : str):
+        super().__init__(nom, prenom) #appel du constructeur du parent
+        #appel des methodes set
+        self.set_telephone(telephone)
+        self.set_courriel(courriel)
+
+    #les methodes d'acces
+    #pour telephone
+    def get_telephone(self):
+        return self.__telephone
+
+    def set_telephone(self, value):
+        self.__telephone = value
+
+    #pour courriel
+    def get_courriel(self):
+        return self.__courriel
+
+    def set_courriel(self, value):
+        self.__courriel = value
+
+
+    #La méthode magique nommée "__str__".
+    def __str__(self):
+        return f'{super().__str__()}\n, {self.__telephone}\n, {self.__courriel}'
+
+
+#creation de la classe reparation
+class Reparation(object):
+
+    #le constructeur
+    def __init__(self, code : int = 0, description : str = 'rien', montant : float = 0, datereparation : datetime = '01/01/1111', codeemploye : int = 0):
+        self.set_code(code)
+        self.set_description(description)
+        self.set_montant(montant)
+        self.set_datereparation(datereparation)
+        self.set_codeemploye(codeemploye)
+
+    #les methodes d'acces
+    #pour code
+    def get_code(self):
+        return self.__code
+
+    def set_code(self, value):
+        self.__code = value
+
+    #pour description
+    def get_description(self):
+        return self.__description
+
+    def set_description(self, value):
+        self.__description = value
+
+    #pour montant
+    def get_montant(self):
+        return self.__montant
+
+    def set_montant(self, value):
+        self.__montant = value
+
+    #pour datereparation
+    def get_datereparation(self):
+        return self.__datereparation
+
+    def set_datereparation(self, value):
+        self.__datereparation = value
+
+    #pour codeemploye
+    def get_codeemploye(self):
+        return self.__codeemploye
+
+    def set_codeemploye(self, value):
+        self.__codeemploye = value
+
 
